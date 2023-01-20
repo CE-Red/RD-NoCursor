5084e9eccb698f850ede73baa98add360fb830b5
CE-Red <122650470+CE-Red@users.noreply.github.com>
2023-01-20 23:18:43 +0100

    Update NoCurs.ps1

--- NoCurs.ps1
+++ NoCurs.ps1
@@ -1,5 +1,15 @@
+# Title: NoCursing
+
+# Author: CE-Red
+
+# Description: A script that turns your cursor invisible and will leave your friends cursing in front of their computers
+
+# Target: Windows 10, 11
+
+# Here is the encoded data for the .zip file containing the new cursor
 $b64 = "UEsDBBQAAAAIAByYNFbqX0SROQAAAL4QAAAJAAAATm9DdXIuY3Vy7cmxDYAwEEPRf0yQivpKxshojJMtkwOGSMF/kmXJhoMgk9docFZflWfqleA774YkSZIkSfqhudkCUEsBAhQAFAAAAAgAHJg0VupfRJE5AAAAvhAAAAkAAAAAAAAAAAAgAAAAAAAAAE5vQ3VyLmN1clBLBQYAAAAAAQABADcAAABgAAAAAAA="
 
+#The .zip file gets created and extracted
 $filename = ($env:temp)+"\Nocur.zip";
 
 $bytes = [Convert]::FromBase64String($b64)
@@ -11,6 +21,8 @@ Expand-Archive $filename -DestinationPath $Env:temp;
 
 Start-Sleep 2;
 
+# This Code is written by zett42
+# Define a C# class for calling WinAPI.
 Add-Type -TypeDefinition @'
 public class SysParamsInfo {
     [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
@@ -26,6 +38,8 @@ public class SysParamsInfo {
 }
 '@
 
+# Change the Registry Key for the Cursor
 Set-ItemProperty -Path 'HKCU:\Control Panel\Cursors' -Name 'Arrow' -Value '%TEMP%\NoCur.cur';
 
+# Notify the system about settings change by calling the C# code
 [SysParamsInfo]::CursorHasChanged()
